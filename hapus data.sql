f8edc87a1c074c407ad3b85d541cb5704cad1c5a
Fala2002 <113907759+Fala2002@users.noreply.github.com>
2022-11-09 21:04:29 +0700

    Update hapus data.sql

--- hapus data.sql
+++ hapus data.sql
@@ -3,4 +3,4 @@ use toko_online;
 delete from pelanggan where id = 2;
 delete from transaksi where id = 8;
 delete from barang where id = 21;
-detele from kategori where id = 5; 
+delete from kategori where id = 5; 
